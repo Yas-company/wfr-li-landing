9909b105c8927061bb4980c489a7dc44a0def765
x7md <me@x7md.net>
2025-06-30 14:06:54 +0300

    fix site base by base

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,8 @@ const __dirname = path.dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://www.yastech.com.sa/wfr-li-landing/",
+    // site: "https://www.yastech.com.sa/wfr-li-landing/",
+    base: "/wfr-li-landing/",
     vite: {
       plugins: [tailwindcss()],
       resolve: {
